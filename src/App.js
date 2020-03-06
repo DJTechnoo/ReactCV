df172e02318b5b32ffb59662984d463bffe39a3e
askelej <askeltechno@hotmail.com>
2020-03-06 02:34:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/UI/NavBar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
+import './App.css';
+import Skills from './components/pages/Skills';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar/>
+
+        <Switch>
+          <Route exact path="/skills" component={Skills}/>
+          <Route exact path="/page2"/>
+          <Route exact path="/page3"/>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
---
+++ src/components/UI/NavBar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap'
+
+const NavBar = () => {
+    return (
+        <>
+        <style type="text/css">
+            {`
+            .nav {
+                padding: 1rem 1.5rem;
+                font-size: 1.5rem;
+                background-color: black;
+                color: white;
+            }
+            .nav-link:hover {
+                background-color: white;
+                color: black;
+                transform: scale(1.1, 1.1);
+            }
+
+            .nav-link {
+                color: white;
+                transition: transform 0.3s;
+            }
+
+            h2 {
+                margin-left: auto;
+            }
+
+            `}
+        </style>
+        <Nav variant="pills">
+            <Nav.Item>
+                <LinkContainer to="skills">
+                    <Nav.Link eventKey="link-1">Skills</Nav.Link>
+                </LinkContainer>
+            </Nav.Item>
+            <Nav.Item>
+                <LinkContainer to="page2">
+                    <Nav.Link eventKey="link-2">Page 2</Nav.Link>
+                </LinkContainer>
+            </Nav.Item>
+            <Nav.Item>
+                <LinkContainer to="page3">
+                    <Nav.Link eventKey="link-3">Page 3</Nav.Link>
+                </LinkContainer>
+            </Nav.Item>
+            <h2>Askel Eirik Johansson</h2>
+        </Nav>
+        </>
+    )
+}
+
+export default NavBar;
